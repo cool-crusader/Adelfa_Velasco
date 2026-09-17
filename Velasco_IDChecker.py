1a91e3035036a63ca9162d57073412633efb852d
cool-crusader <sevelasco@carc.pshs.edu.ph>
2026-09-17 18:29:33 +0800

    Add files via upload

---
+++ Velasco_IDChecker.py
@@ -0,0 +1,13 @@
+# Sean Gabriel Velasco
+# 8 - Adelfa
+
+import re
+
+student_id = input("Enter Student ID: ")
+
+pattern = r"\d{4}-\d{4}"
+
+if re.fullmatch(pattern, student_id):
+    print("Valid Student ID.")
+else:
+    print("Invalid Student ID.")
