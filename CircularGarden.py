62623bad348cf55ac83fa58296807d9471f183ab
cool-crusader <sevelasco@carc.pshs.edu.ph>
2026-09-19 12:35:54 +0800

    Refactor CircularGarden.py and add README.md
    Updated CircularGarden.py with comments and improved output formatting. Added README.md for project description and usage instructions.

--- CircularGarden.py
+++ CircularGarden.py
@@ -22,3 +22,27 @@ print(f"The Circumference of the Circular Garden is: {circumference:.2f} meters"
 print(f"The Square of the Circular Garden is: {squareRoot:.2f}")
 print(f"The rounded Up Area is: {roundedUp:.2f} square meters")
 print(f"The rounded Down Area is: {roundedDown:.2f} square meters")
+
+README.md
+# Project Title: Circular Garden Calculator
+
+## Description: Everything About the Program
+This program calculates the Area, Circumference, the Square Root, the Area rounded up and down of a Circular Garden depending on the radius you enter.
+
+## How to run the program
+Input your Radius Measurement
+
+## Input needed
+The only Input you need is the radius of the circle
+
+## Output Examples
+Please enter your radius: 5
+The Area of the Circular Garden is: , 78.54 square meters
+The Circumference of the Circular Garden is: 31.42 meters
+The Square of the Circular Garden is: 8.86
+The rounded Up Area is: 79.00 square meters
+The rounded Down Area is: 78.00 square meters
+
+## Author:
+Name: Sean Gabriel E. Velasco
+Grade and Section: 8-Adelfa
